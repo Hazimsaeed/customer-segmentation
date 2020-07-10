deb64eaf5abbd88e8774ac7397dea7b302c38697
Hazimsaeed <33011527+Hazimsaeed@users.noreply.github.com>
2020-07-09 23:34:17 -0400

    Update clustreg.predict.R

--- clustreg.predict.R
+++ clustreg.predict.R
@@ -2,7 +2,7 @@ clustreg.predict=function(results,newdat){
   
   yhat=rep(NA,nrow(newdat))
   resid=pred=matrix(0,nrow(newdat),length(table(results$cluster)))
-  
+   
   for(j in 1:length(table(results$cluster))){			
     pred[,j]=predict(glm(results$data[results$cluster==j,],family="gaussian"),newdata=newdat)		
     resid[,j] = (pred[,j]-newdat[,1])^2
